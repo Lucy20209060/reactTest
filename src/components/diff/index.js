d883c8f7778678b3b1547f77f0ae13fada49e2bc
luc <550502661@qq.com>
2017-12-14 01:00:40 +0800

    lu

--- src/components/diff/index.js
+++ src/components/diff/index.js
@@ -2,15 +2,35 @@ import React from 'react';
 
 import './index.css'
 
-import json from './data'
-// console.log(json('res','a','a'))
+import json from './index.json'
+console.log(json)
 
 export default class Diff extends React.Component{
+
+	constructor(props) {  
+        super(props);   
+        this.state = {  
+            sign:0
+        };  
+    }  
+
+	//耗时操作放在这里面   
+    componentDidMount() {
+        this.setState({
+			sign: 2
+		});
+    }
+
+	fun(){
+		return 	this.state.sign == 1 
+				? <div>aaa</div>
+				: <div>bbbb</div>
+	}
 
 	render(){ 
 		return (
 		  	<div>
-		  		diff
+		  		{this.fun()}
 		  	</div>
 		) 
 	}
