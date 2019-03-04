b6e79e8cb886b6f31ba5b3402704e0c235b86d56
Lucy20209060 <550502661@qq.com>
2019-03-04 15:14:38 +0800

    没有什么修改

--- src/components/dnd/index.js
+++ src/components/dnd/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './index.css'
 // import {parseDiff, Diff, Hunk} from 'react-diff-view';
+import { Tabs } from 'antd';
 
+const TabPane = Tabs.TabPane;
 
 export default class Dnd extends React.Component{
 
 	// 设置默认状态 可以修改 this.state
 	constructor(props) {
-        super(props);
-        this.state = {};
-    }
+		super(props);
+		this.state = {};
+  }
 
-    // 组件即将渲染
-    componentWillMount() {
-        // class sup {
+  // 组件即将渲染
+  componentWillMount() {
+    // class sup {
 		// 	constructor(name) {
 		// 		this.name = name
 		// 	}
@@ -40,85 +42,80 @@ export default class Dnd extends React.Component{
 		// 	jack.printAge()    //输出 : 20
 		// }
 
-// 构造函数
-// function Person(name){
-// 	this.name = name;
-// 	this.sleep = function() {
-// 		console.log('人以睡为天')
-// 	}
-// }
-// Person.prototype.eat = function() {
-//     console.log(this.name + '正在吃饭')
-// }
+		// 构造函数
+		// function Person(name){
+		// 	this.name = name;
+		// 	this.sleep = function() {
+		// 		console.log('人以睡为天')
+		// 	}
+		// }
+		// Person.prototype.eat = function() {
+		//     console.log(this.name + '正在吃饭')
+		// }
 
-// function Chinese(name){
-// 	Person.call(this);
-// 	this.name = name;
-// }
-// (function(){
-// 	// 创建一个没有实例方法的类
-// 	var Super = function(){};
-// 	Super.prototype = Person.prototype;
-// 	//将实例作为子类的原型
-// 	Chinese.prototype = new Super();
-// })();
-// Chinese.prototype.constructor = Chinese;
+		// function Chinese(name){
+		// 	Person.call(this);
+		// 	this.name = name;
+		// }
+		// (function(){
+		// 	// 创建一个没有实例方法的类
+		// 	var Super = function(){};
+		// 	Super.prototype = Person.prototype;
+		// 	//将实例作为子类的原型
+		// 	Chinese.prototype = new Super();
+		// })();
+		// Chinese.prototype.constructor = Chinese;
 
-// var lucy = new Chinese('lucy');
-// console.log(lucy)
-// console.log(lucy.name); // lucy
-// lucy.eat(); // lucy正在吃饭
-// lucy.sleep(); // 人以睡为天
-// console.log(lucy instanceof Person); // true
-// console.log(lucy instanceof Chinese); //true
-
-
-
-// 实例化一个lucy对象
-// const lucy = new Person('Lucy');
-// lucy.eat(); // Lucy正在吃饭
-
-// function Dog(name){
-// 	Person.call(this);
-// 	this.name = name;
-// }
-// Dog.prototype.speak = function() {
-// 	console.log(this.name + '汪汪')
-// }
-
-// const dog = new Dog('旺财');
-// // dog.eat()
-// console.log(dog)
-
-class Person {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
-class Chinese extends Person {
-	constructor(x, y, color) {
-		super(x, y); // 调用父类的constructor(x, y)
-		this.color = color;
-	}
-
-	toString() {
-		return this.color + ' ' + super.toString(); // 调用父类的toString()
-	}
-}
+		// var lucy = new Chinese('lucy');
+		// console.log(lucy)
+		// console.log(lucy.name); // lucy
+		// lucy.eat(); // lucy正在吃饭
+		// lucy.sleep(); // 人以睡为天
+		// console.log(lucy instanceof Person); // true
+		// console.log(lucy instanceof Chinese); //true
 
 
 
+		// 实例化一个lucy对象
+		// const lucy = new Person('Lucy');
+		// lucy.eat(); // Lucy正在吃饭
 
+		// function Dog(name){
+		// 	Person.call(this);
+		// 	this.name = name;
+		// }
+		// Dog.prototype.speak = function() {
+		// 	console.log(this.name + '汪汪')
+		// }
+
+		// const dog = new Dog('旺财');
+		// // dog.eat()
+		// console.log(dog)
+
+		class Person {
+			constructor(x, y) {
+				this.x = x;
+				this.y = y;
+			}
+		}
+
+		class Chinese extends Person {
+			constructor(x, y, color) {
+				super(x, y); // 调用父类的constructor(x, y)
+				this.color = color;
+			}
+
+			toString() {
+				return this.color + ' ' + super.toString(); // 调用父类的toString()
+			}
+		}
 	}
 
 
 	render(){
-		
 		return (
 			<div>
-				dnd
+				哈啊哈哈哈
 			</div>
 		) 
 	}
